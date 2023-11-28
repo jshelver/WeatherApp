b7e76d2ba38292b07aeb8b004273ec34577217e9
jason <97709845+jshelver@users.noreply.github.com>
2023-11-28 16:03:10 -0600

    API Call to OpenWeather

--- server/src/routes/api/Routes.js
+++ server/src/routes/api/Routes.js
@@ -1,13 +1,31 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
+import https from 'https';
 
 import dotenv from 'dotenv';
 dotenv.config({ path: '../../../../.env'});
 
-
+const BASE_API_URL = "https://api.openweathermap.org/data/2.5/weather?";
 const router = Router();
 
 router.get('/test', async (req, res) => {
-    return res.status(200).json({ message: 'Test route works!' });
+    try {
+        https.get(`${BASE_API_URL}q=London&appid=${process.env.OPEN_WEATHER_API_KEY}`,
+            (resp) => {
+                let data = '';
+                resp.on('data', (chunk) => {
+                    data += chunk;
+                });
+                resp.on('end', () => {
+                    const jsonData = JSON.parse(data);
+                    return res.status(200).json({ weatherData: jsonData });
+                });
+            }).on("error", (err) => {
+                console.log("Error: " + err.message);
+            }
+        );
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
 });
 
 module.exports = router;
